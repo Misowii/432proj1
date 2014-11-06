1c66fa684ea5c26671fb25045184b67c3d6422f7
Brandenj <brandenj456@gmail.com>
2014-11-06 14:44:46 -0800

    commented code

--- server.c
+++ server.c
@@ -16,17 +16,18 @@
 int Run = 1;
 char *serverAddr;
 int serverPort;
-char message[SAY_MAX];
 int recvlen;
 int serverSocket;
-unsigned char sendmessage;
-#define MAX_USERS 100
-#define MAX_CHANNELS 32 
+#define MAX_USERS 100 //max users that can connect
+#define MAX_CHANNELS 32  //max channels open
 int usercount = 0;
 socklen_t addrlen;
 
 struct hostent *hp;
+struct sockaddr_in hostaddr;		
+struct sockaddr remaddr;
 
+//Attempts at data structures for keeping track of channels and users
 struct User{
 	char *cur_username;
 	//struct Channel cur_channels[MAX_CHANNELS];
@@ -46,8 +47,7 @@ struct Channel all_Channels[MAX_CHANNELS];
 struct Channel Common;
 
 
-struct sockaddr_in hostaddr;		
-struct sockaddr remaddr;
+
 
 void list(struct request reqmes){
 	printf("list\n");
@@ -67,45 +67,18 @@ void say(struct request reqmes){
 	struct request_say *reqsay;
 	reqsay = (struct request_say *)&reqmes;
 }
-	/*int i;
-	int f = chan.numusers;
-	for (i = 0; i < f; i++){
-		struct User utemp = Channel.cur_usernames[i];
-		struct sockaddr temp = utemp.cur_address;
 
-		sendto(serverSocket, mesg, strlen(mesg), 0, (struct sockaddr *)&temp, sizeof(temp));
-	}
-
-}
-*/
 
 void login(struct request reqmes){
 	printf("join\n");
 	struct request_login *reqlogin;
 	reqlogin = (struct request_login *)&reqmes;
-
-	/*//printf("%s\n", username);
-	struct User user;
-	user.cur_username = username;
-	user.cur_address = remaddr;
-	//user.cur_channels[0] = Common;
-	user.numchannels += 1;
-	all_Users[0] = user;
-	int i;
-	i = Common.numusers;
-	Common.cur_usernames[i] = user;
-	Common.numusers += 1;
-	//all_Users[0].cur_address = remaddr;
-	usercount += 1;
-	*/
-
 }
 
 void join(struct request reqmes){
 	printf("join\n");
 	struct request_join *reqjoin;
 	reqjoin = (struct request_join *)&reqmes;
-
 
 }
 void logout(struct request reqmes){
@@ -120,12 +93,14 @@ void leave(struct request reqmes){
 	reqleave = (struct request_leave *)&reqmes;
 }
 
+
+//Once recvfrom works it sends the basic request struct to here for
+//further deciphering.
+//All login/logout/say/join/who/list/leave functions just print
+//what function they are. (for now)
 void recieve(struct request request_message){
 	printf("got to recieve\n");
-	//struct request request_message;
 
-	//addrlen = sizeof(remaddr);
-	//recvlen = recvfrom(serverSocket, &request_message, sizeof(request_message), 0, (struct sockaddr *)&remaddr, &addrlen );
 	if (request_message.req_type == REQ_LOGIN){
 		login(request_message);
 	}
@@ -158,22 +133,17 @@ int main(int argc, char *argv[])
 		printf("Server requires 2 arguments. Address and Port.\n");
 		return(-1);
 
-	}
+	}	
+		//creates the first channel and adds it to list of active channels
+		//Also sets the channel common to have 0 users on it.
 		all_Channels[0] = Common;
 		Common.numusers = 0;
 
 		serverAddr = argv[1];
 		serverPort = atoi(argv[2]);
 
-		//printf("%s\n", serverAddr);
-		//printf("%d\n", serverPort);
-/*
-		memset((char *)&hostaddr, 0, sizeof(hostaddr));
-		hostaddr.sin_family = AF_INET;
-		hostaddr.sin_addr.s_addr = inet_addr(serverAddr);
-		//hostaddr.sin_addr.s_addr = htonl(INADDR_ANY); //I DONT KNOW!!!!
-		hostaddr.sin_port = htons(serverPort);
-*/
+		//Setting up server information and socket
+ 
 		hp = gethostbyname(serverAddr);
 
 		memset((char *)&hostaddr, 0, sizeof(hostaddr));
@@ -193,10 +163,26 @@ int main(int argc, char *argv[])
 			return(-1);
 		};
 
-
+		//loop while waiting for messages?
 		
 		printf("waiting for connection\n");
 		while(Run == 1){
+
+
+		//Attempting to recieve a message then pass the request struct to
+		//recieve() to decifer the type.
+
+		struct request request_message;	
+		addrlen = sizeof(remaddr);
+		int temp;
+		temp = recvfrom(serverSocket, &request_message, sizeof(request_message), 0, (struct sockaddr *)&remaddr, &addrlen);
+		if ( temp > 0 ){
+			recieve(request_message);
+
+
+		//Attempt at select() for server. Might not need to use select
+		// for the server.	
+
 		/*struct timeval tv;
 		fd_set readfds;
 		fd_set master;
@@ -219,19 +205,10 @@ int main(int argc, char *argv[])
 			//for (i = 0; i <= fdmax; i++){	
 			//	if (FD_ISSET(i, &readfds)){
 			//		printf("connection\n");
-		struct request request_message;	
-		addrlen = sizeof(remaddr);
-		int temp;
-		temp = recvfrom(serverSocket, &request_message, sizeof(request_message), 0, (struct sockaddr *)&remaddr, &addrlen);
-		if ( temp > 0 ){
-			recieve(request_message);
+
+
+
 		}
-		//else if	(temp == -1){
-			//perror("recvfrom");
-			
-		//}
-				//	}
-			//}
 		};
 	close(serverSocket);
 	return 0;
